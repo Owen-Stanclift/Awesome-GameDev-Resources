3173f02d884f5ddd8eca3017769ec4a4bb5b8cac
Owen Stanclift <owen.stanclift@mymail.champlain.edu>
2024-09-17 15:41:55 -0400

    Added mersenneTwister

--- docs/artificialintelligence/assignments/rng/rng.cpp
+++ docs/artificialintelligence/assignments/rng/rng.cpp
@@ -1,18 +1,24 @@
 // add your imports here
+#include <cstdint>
 #include <fstream>
 #include <iostream>
 #include <istream>
+#include <unordered_set>
+#include<random>
 const std::string TEST_FOLDER = "\\tests\\";
 unsigned int xorShift(unsigned int seed, int r1, int r2);
+unsigned int mersenneTwister(unsigned int seed, int r1, int r2);
+unsigned int seed, N, min, max;
 int main(){
   // code here
-  unsigned int seed, N, min, max;
+
   std::cin >> seed >> N >> min >> max;
   unsigned int i;
   for(i = N; i >= 1; i--)
   {
     //Run xor shift
     seed = xorShift(seed, min, max);
+    seed = mersenneTwister(seed, min, max);
   }
 }
 //The purpose of this function is to take the number and xor shift it to output a pseudo-random number
@@ -25,4 +31,15 @@ int main(){
           //output the new values
           std::cout << value << std::endl;
   return seed;
+}
+
+unsigned int mersenneTwister(unsigned int seed,int r1, int r2) {
+  seed ^=  (seed << 11);
+  seed ^= (seed >> 7) & 0x9d2c5680UL;
+  seed ^= (seed << 15) & 0xefc60000UL;
+  unsigned int newSeed = seed ^ (seed >> 18);
+  int value = r1 + (newSeed % (r2 - r1 + 1)); //clamps the value to between r1 and r2
+  //output the new values
+  std::cout << value << std::endl;
+  return newSeed;
 }
